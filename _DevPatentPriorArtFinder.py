6e686554fb2c7395b0911a1911af76d70ee4d2cb
EphraimMeiri <ephraimmeiri@gmail.com>
2021-07-01 11:18:15 -0400

    Added matches method

--- _DevPatentPriorArtFinder.py
+++ _DevPatentPriorArtFinder.py
@@ -417,9 +417,19 @@ class _DevPatentPriorArtFinder:
 
         return new_complete_df
 
-
-
-
-
-
-
+    def matches(self,dataframe):
+        r = 0
+        for index, row in dataframe.head((len(dataframe)/2)+1).iterrows():
+            n = 0
+            for entry in row:
+                if type(entry) is not str and entry < .99 and entry >= .6:
+                    print(entry)
+                    print(index)
+                    print(dataframe['Publication_Number'][n])
+                    print("col: " + str(n))
+                    print("row: " + str(r))
+                    print(dataframe['Abstract'][n])
+                    print(dataframe['Abstract'][r])
+                    print()
+                n += 1
+            r += 1
